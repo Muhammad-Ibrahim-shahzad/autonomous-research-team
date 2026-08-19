78c3ff429121bca8d9e9f8ee3c211267a3a58503
Muhammad-Ibrahim-shahzad <fa24-bce-048@cuilahore.edu.pk>
2026-08-19 18:27:49 +0500

    Analysis node

--- agents.py
+++ agents.py
@@ -23,12 +23,54 @@ def research_node(state: MultiAgentState) -> dict:
     search_results = tavily_client.search(query=query)
     results = search_results["results"]
 
-    draft_system_prompt = """
-    You are a research assistant, answer the question using the search results below.
+    research_system_prompt = """
+    You are a research agent. Your job is to summarize the key facts and 
+    findings from the search results below — DO NOT to write the final answer 
+    or conclude anything from the search. Just organize what was found: key facts,
+    numbers, dates, and claims, clearly and neutrally.
     """
+
     messages = [
         {
             "role": "system",
-            "content": draft_system_prompt
+            "content": research_system_prompt
+        },
+        {
+            "role": "user",
+            "content": f"Search Results: {results}\n\nOriginal Query: {state['query']}"            
         }
     ]
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=messages
+    )
+    answer = response.choices[0].message.content
+
+    return {"search_results": results, "research_answer": answer}
+
+def analysis_node(state: MultiAgentState) -> dict:
+
+    analysis_system_prompt = """
+    You are a professional analyst. You will be given search results and
+    a research summary. Your work is to analyze it properly, organize it 
+    in a proper manner, and highlight the most important information.
+    DO NOT include anything unnecessary in the final answer. 
+    """
+
+    messages = [
+        {
+            "role": "system",
+            "content": analysis_system_prompt
+        },
+        {
+            "role": "user",
+            "content": f"Search Results: {state['search_results']}\n\nResearch Summary: {state['research_answer']}"
+        }
+    ]
+    response = client.chat.completions.create(
+        model="openai/gpt-oss-120b",
+        messages=messages
+    )
+    answer = response.choices[0].message.content
+
+    return {"analysis_answer": answer}
